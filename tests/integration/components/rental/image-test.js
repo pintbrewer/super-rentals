efb462b0e7a9e446733d2eec2e352571ede7d412
pintbrewer <pintbrewer>
2022-01-13 16:35:21 -0500

    Interactive Componenets
    https://guides.emberjs.com/release/tutorial/part-1/interactive-components/

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function (hooks) {
@@ -12,10 +12,35 @@ module('Integration | Component | rental/image', function (hooks) {
     alt="Teaching Tomster"
   />
 `);
-assert
+    assert
       .dom('.image img')
       .exists()
       .hasAttribute('src', '/assets/images/teaching-tomster.png')
       .hasAttribute('alt', 'Teaching Tomster');
+  });
+  test('clicking on the component toggles size', async function (assert) {
+    // Make sure exeists
+    //make sure is not large
+    // has text View Larger
+    // on click is large
+    // has text View Smaller
+    // on click is not large
+    // has text View Larger
+    setupRenderingTest(hooks);
+    await render(hbs`
+      <Rental::Image
+      src="/assets/images/teaching-tomster.png"
+      alt="Teaching Tomster"
+      />
+    `);
+    assert.dom('button.image').exists();
+    assert.dom('.image').doesNotHaveClass('large')
+    assert.dom('.image').hasText('View Larger');
+    await click('button.image');
+    assert.dom('.image').hasClass('large');
+    assert.dom('.image').hasText('View Smaller')
+    await click('button.image');
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image').hasText('View Larger');
   });
 });
