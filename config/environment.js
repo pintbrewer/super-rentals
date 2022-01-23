d0160fa136836bcb948ceed8ab2c64a1f4e22be3
pintbrewer <pintbrewer>
2022-01-22 21:11:20 -0500

    change var name

--- config/environment.js
+++ config/environment.js
@@ -46,7 +46,6 @@ module.exports = function (environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
-  ENV.MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
-
+  ENV.MAPBOX_ACCESS_TOKEN = process.env.MY_MAPBOX_ACCESS_TOKEN;
   return ENV;
 };
